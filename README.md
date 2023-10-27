ff0b3576574ec2e2c02afe86983e5659b6e28333
Amphiprions <amphiprions@gmail.com>
2023-10-27 14:35:17 +0200

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Purpose
+The goal of this script is to let you keeping some orginal text based on their key names (using regular expression to match them) and so ovveride the translation existing in the translated/global.ini file
+
+For example to avoid translating all Stanton element except their respective description ypu can use this regex:
+^(Stanton)(?!.*_desc.*)
+
+This regexp will match all global.ini keys starting with "Stanton" and not containing "_desc" somewhere after that
+
+# Configuration
+1) Copy the most up-to-date global.ini (CIG original file) in the original directory
+2) Copy your preferred translated global.ini file in the translated directory 
+3) Edit the do-not-translate-pattern.ini file containing regex pattern defining what to keep as key name in the original file (key name can be anything, value are regexp)
+# Powershell authorization
+1) Open powershell and execute this command
+2) Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
+# Script execution
+1) Now you can execute do-not-translate.ps1 scripts
+2) the generated global.ini file will be aside the ps1 script
---
+++ do-not-translate.ps1
@@ -0,0 +1,42 @@
+function Get-IniContent ($filePath)
+{
+	$ini = @{}
+	switch -regex -file $FilePath
+	{
+    	"(.+?)\s*=(.*)" # Key
+    	{
+        	$name,$value = $matches[1..2]
+        	$ini[$name] = $value
+    	}
+	}
+	return $ini
+}
+$ORIGINAL_INI = Get-IniContent "original/global.ini"
+$TRANSLATED_INI = Get-IniContent "translated/global.ini"
+$PATTERN_INI = Get-IniContent "do-not-translate-pattern.ini"
+
+$PATTERN_INI.GetEnumerator() | ForEach-Object{
+    if (!$_.key.StartsWith(";")) {
+        $patternKeepOriginal = $_.value
+        # no look if we need to keep the original text
+        $message = "Searching keys in orginal file matching {0} in order to keep them as is..." -f $patternKeepOriginal
+        Write-Output $message
+        $count = 0
+        $ORIGINAL_INI.GetEnumerator() | ForEach-Object{
+            if ($_.key -cmatch $patternKeepOriginal) {
+                $count = $count + 1
+                $TRANSLATED_INI[$_.key] = $_.value
+            }
+        }
+        $message = "Keeping {0} keys" -f $count
+        Write-Output $message
+    }
+}
+Out-File -FilePath "global.ini"
+#New-Item -Name global.ini -ItemType File -Force
+Write-Output "writing result in ./global.ini, it will take some times..."
+$TRANSLATED_INI.GetEnumerator() | Sort-Object -Property key | ForEach-Object {
+    $message = "{0}={1}" -f $_.key,$_.value
+    $message >> "./global.ini"
+}
+
